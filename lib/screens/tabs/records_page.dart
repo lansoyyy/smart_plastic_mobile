eb402dd1db3971bdab232c777b30bd5049232b31
Lans12345 <olanalans12345@gmail.com>
2024-04-18 09:31:55 +0800

    added records tab ui

--- lib/screens/tabs/records_page.dart
+++ lib/screens/tabs/records_page.dart
@@ -21,6 +21,87 @@ class RecordsPage extends StatelessWidget {
           color: Colors.white,
         ),
       ),
+      body: SingleChildScrollView(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            showCheckboxColumn: false,
+            border: TableBorder.all(),
+            columnSpacing: 50,
+            columns: [
+              DataColumn(
+                label: TextWidget(
+                  text: 'ID Number',
+                  fontSize: 18,
+                  fontFamily: 'Bold',
+                ),
+              ),
+              DataColumn(
+                label: TextWidget(
+                  text: 'Item Name',
+                  fontSize: 18,
+                  fontFamily: 'Bold',
+                ),
+              ),
+              DataColumn(
+                label: TextWidget(
+                  text: 'Item Points',
+                  fontSize: 18,
+                  fontFamily: 'Bold',
+                ),
+              ),
+              DataColumn(
+                label: TextWidget(
+                  text: 'Date and Time',
+                  fontSize: 18,
+                  fontFamily: 'Bold',
+                ),
+              ),
+            ],
+            rows: [
+              for (int i = 0; i < 100; i++)
+                DataRow(
+                    color: MaterialStateColor.resolveWith(
+                      (states) => i % 2 == 0 ? Colors.white : Colors.grey[200]!,
+                    ),
+                    cells: [
+                      DataCell(
+                        TextWidget(
+                          text: '${i + 1}',
+                          fontSize: 14,
+                          fontFamily: 'Medium',
+                          color: Colors.grey,
+                        ),
+                      ),
+                      DataCell(
+                        TextWidget(
+                          text: 'Sardines',
+                          fontSize: 14,
+                          fontFamily: 'Medium',
+                          color: Colors.grey,
+                        ),
+                      ),
+                      DataCell(
+                        TextWidget(
+                          text: '150 pts',
+                          fontSize: 14,
+                          fontFamily: 'Medium',
+                          color: Colors.grey,
+                        ),
+                      ),
+                      DataCell(
+                        TextWidget(
+                          text: DateTime.now().toString(),
+                          fontSize: 14,
+                          fontFamily: 'Medium',
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ])
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/tabs/redeem_page.dart
+++ lib/screens/tabs/redeem_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_plastic_mobile/utlis/colors.dart';
+import 'package:smart_plastic_mobile/widgets/button_widget.dart';
 import 'package:smart_plastic_mobile/widgets/drawer_widget.dart';
 import 'package:smart_plastic_mobile/widgets/text_widget.dart';
 
@@ -19,6 +20,173 @@ class RedeemPage extends StatelessWidget {
           fontSize: 18,
           fontFamily: 'Bold',
           color: Colors.white,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: TextWidget(
+                text: 'Total Points: 150 pts',
+                fontSize: 18,
+                color: secondary,
+                fontFamily: 'Bold',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 3,
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextWidget(
+                          text: 'Item Name',
+                          fontSize: 11,
+                          color: grey,
+                        ),
+                        TextWidget(
+                          text: 'Sardines',
+                          fontSize: 32,
+                          color: secondary,
+                          fontFamily: 'Bold',
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextWidget(
+                          text: 'Equivalent points',
+                          fontSize: 10,
+                          color: grey,
+                        ),
+                        TextWidget(
+                          text: '125 pts',
+                          fontSize: 18,
+                          color: secondary,
+                          fontFamily: 'Bold',
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        ButtonWidget(
+                          radius: 100,
+                          height: 35,
+                          width: 125,
+                          fontSize: 12,
+                          label: 'REDEEM',
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                      title: const Text(
+                                        'Redeem Confirmation',
+                                        style: TextStyle(
+                                            fontFamily: 'QBold',
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      content: const Text(
+                                        'Are you sure you want to redeem this item?',
+                                        style:
+                                            TextStyle(fontFamily: 'QRegular'),
+                                      ),
+                                      actions: <Widget>[
+                                        MaterialButton(
+                                          onPressed: () =>
+                                              Navigator.of(context).pop(true),
+                                          child: const Text(
+                                            'Close',
+                                            style: TextStyle(
+                                                fontFamily: 'QRegular',
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                        MaterialButton(
+                                          onPressed: () async {
+                                            Navigator.of(context).pop();
+                                            showDialog(
+                                                barrierDismissible: false,
+                                                context: context,
+                                                builder: (context) =>
+                                                    AlertDialog(
+                                                      title: const Column(
+                                                        mainAxisSize:
+                                                            MainAxisSize.min,
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .center,
+                                                        crossAxisAlignment:
+                                                            CrossAxisAlignment
+                                                                .center,
+                                                        children: [
+                                                          Icon(
+                                                            Icons
+                                                                .check_circle_outline,
+                                                            color: Colors.green,
+                                                            size: 150,
+                                                          ),
+                                                          SizedBox(
+                                                            height: 10,
+                                                          ),
+                                                          Text(
+                                                            'You have succesfully redeemed this item!',
+                                                            style: TextStyle(
+                                                                fontSize: 18,
+                                                                fontFamily:
+                                                                    'Bold',
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .bold),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                      actions: <Widget>[
+                                                        MaterialButton(
+                                                          onPressed: () async {
+                                                            Navigator.of(
+                                                                    context)
+                                                                .pop();
+                                                          },
+                                                          child: const Text(
+                                                            'Continue',
+                                                            style: TextStyle(
+                                                                fontFamily:
+                                                                    'QRegular',
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .bold),
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ));
+                                          },
+                                          child: const Text(
+                                            'Continue',
+                                            style: TextStyle(
+                                                fontFamily: 'QRegular',
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                      ],
+                                    ));
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
