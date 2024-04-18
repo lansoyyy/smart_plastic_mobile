6fb976522eccb74167a7e5f0be96493373e0bcbe
Lans12345 <olanalans12345@gmail.com>
2024-04-18 09:20:25 +0800

    added home screen ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smart_plastic_mobile/screens/tabs/redeem_page.dart';
 import 'package:smart_plastic_mobile/utlis/colors.dart';
+import 'package:smart_plastic_mobile/widgets/button_widget.dart';
 import 'package:smart_plastic_mobile/widgets/drawer_widget.dart';
 import 'package:smart_plastic_mobile/widgets/text_widget.dart';
 
@@ -20,6 +22,89 @@ class HomeScreen extends StatelessWidget {
           fontFamily: 'Bold',
           color: Colors.white,
         ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                elevation: 5,
+                child: Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: CircleAvatar(
+                                minRadius: 50,
+                                maxRadius: 50,
+                                backgroundImage: NetworkImage(
+                                  '',
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                TextWidget(
+                                  text: 'John Doe',
+                                  fontSize: 24,
+                                  fontFamily: 'Bold',
+                                ),
+                                TextWidget(
+                                  text: 'jhondoe@gmail.com',
+                                  fontSize: 14,
+                                  fontFamily: 'Medium',
+                                  color: grey,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        TextWidget(
+                          decoration: TextDecoration.underline,
+                          text: '150 pts',
+                          fontSize: 45,
+                          fontFamily: 'Bold',
+                          color: secondary,
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ButtonWidget(
+            label: 'REDEEM',
+            onPressed: () {
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => const RedeemPage()));
+            },
+          ),
+        ],
       ),
     );
   }
